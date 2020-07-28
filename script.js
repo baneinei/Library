c8b264d704b7e96527528e05c3a1cdae2fc1a8e0
victor <westlundvictor8@gmail.com>
2020-07-28 13:52:46 +0200

    Added pretty much the rest, some things need to be fixed still: Make the form minimizable without submitting, also fixing some responsive issues.

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const submitForm = document.querySelector("#submit_form");
 var bookForm = document.querySelector("#submit_form");
 
 bookForm.addEventListener("click", () => {
-    let new_book = new Book(document.querySelector("#book_title").value);
+    let new_book = new Book(document.querySelector("#book_title").value,
+                            document.querySelector("#book_author").value,
+                            document.querySelector("#book_pages").value,
+                            document.querySelector("#book_read").checked);
     addBookToLibrary(new_book);
     toggleDisplay(addBookForm);
     render();
@@ -26,8 +29,16 @@ function toggleDisplay(element) {
     
 }
 
-function Book(name) {
+function Book(name,author,pages,read) {
     this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.toggleRead = function () {
+    this.read = !(this.read);
+    render();
 }
 
 function addBookToLibrary(inputBook) {
@@ -37,21 +48,72 @@ function addBookToLibrary(inputBook) {
 function render() {
     container.innerHTML = "";
     for (let i = 0 ; i < myLibrary.length ; i++) {
+        // creates the remove button, adds the event listener and adds it to the div.
+        let btn = document.createElement("button");
+        btn.textContent = "x";
+        btn.classList.add("remove_book");
+        btn.addEventListener("click", () => {
+            myLibrary.splice(i,1);
+            render();
+        });
+        
+
+        let readBtn = document.createElement("button");
+        readBtn.textContent = "r";
+        readBtn.classList.add("toggle_read");
+        readBtn.addEventListener("click", () => {
+            myLibrary[i].toggleRead();
+        });
+        
+
+
+        //create a div in which the info will be contained
         let div = document.createElement("div");
         div.classList.add("book_card");
-        div.setAttribute("value",i);
+        let p = document.createElement("p");
+        p.textContent = myLibrary[i].name;
+        p.setAttribute("id","title");
+        div.appendChild(readBtn);
+        div.appendChild(btn);
+        div.appendChild(document.createElement("br"));
+        div.appendChild(p);
+       
 
-        div.textContent = myLibrary[i].name;
+        let p1 = document.createElement("p");
+        p1.textContent = myLibrary[i].author;
+        p1.setAttribute("id","author");
+        div.appendChild(p1);
+
+        let p2 = document.createElement("p");
+        p2.textContent = "Pages: " + myLibrary[i].pages;
+        p2.setAttribute("id","pages");
+        div.appendChild(p2);
+
+        let p3 = document.createElement("p");
+        p3.setAttribute("id","read");
+        if (myLibrary[i].read) {
+            p3.textContent = "I have read this book.";
+        }
+        else {
+            p3.textContent = "I have not read this book.";
+        }
+        div.appendChild(p3);
+
+        
+
+
+        //add the div to the container
         container.appendChild(div);
     }
 }
 
-let harry_potter = new Book("Harry Potter");
+let harry_potter = new Book("Harry Potter","JK Rowling",233,true);
 addBookToLibrary(harry_potter);
 
-let lotr = new Book("Lord of the rings");
+
+let lotr = new Book("Lord of the rings","Tolkien",533,false);
 addBookToLibrary(lotr);
 
-let fermat = new Book("Fermat's last theorem");
+let fermat = new Book("Fermat's last theorem","Simon Singh",352,true);
 addBookToLibrary(fermat);
 render();
